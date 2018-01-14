cfa74416dd805db166a5a41c5395fd4d190ca169
pablopunk <pablovarela182@gmail.com>
2018-01-14 23:51:38 +0100

    Fix permissions

--- src/posts/permissions.py
+++ src/posts/permissions.py
@@ -6,8 +6,10 @@ class PostPermissions(BasePermission):
     def has_permission(self, request, view):
         from posts.api import PostCreateAPI
 
+        if request.method == 'GET' and view.get_queryset() is not None and len(view.get_queryset()) != 0:
+            return True
+
         if request.method == 'PUT' or request.method == 'DELETE':
-            print(view)
-            return request.user.is_superuser or view.queryset[0].user == request.user
+            return request.user.is_superuser or view.get_queryset()[0].user == request.user
 
         return request.user.is_authenticated and request.method == 'POST' and isinstance(view, PostCreateAPI)
